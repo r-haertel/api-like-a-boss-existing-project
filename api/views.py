50bdcaa99ee0a85e31828f7c3ea622d60ae57bfe
rhaertel <renanhaertel@gmail.com>
2021-11-28 18:28:54 -0300

    Adds signup and login logic

--- api/views.py
+++ api/views.py
@@ -1,7 +1,38 @@
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import generics, permissions
 from .serializers import TodoSerializer, TodoCompleteSerializer
 from todo.models import Todo
 from django.utils import timezone
+from rest_framework.parsers import JSONParser
+from django.db import IntegrityError
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request)
+            user = User.objects.create_user(request.POST['username'], password=request.POST['password'])
+            user.save()
+            token = Token.objects.create(user=user)
+            return JsonResponse({'token': str(token)}, status=201)
+        except IntegrityError:
+            return JsonResponse({'error':'That username has already been taken. Please choose a new username'}, status=400)
+
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        user = authenticate(request, username=data['username'], password=data['password'])
+        if user is None:
+            return JsonResponse({'error':'Could not login. Please check username and password.'}, status=400)
+        else:
+            token = Token.objects.get_or_create(user=user)
+            return JsonResponse({'token': str(token)}, status=200)
+
 
 class TodoCompletedList(generics.ListAPIView):
     serializer_class = TodoSerializer
