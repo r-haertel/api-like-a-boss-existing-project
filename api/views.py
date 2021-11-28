ffaed494ebe322ac3e3b220938bcd17db376fedf
rhaertel <renanhaertel@gmail.com>
2021-11-28 17:29:56 -0300

    Adds api logic to get a list of tasks

--- api/views.py
+++ api/views.py
@@ -3,9 +3,19 @@ from todo import serializers
 from todo.models import Todo
 
 class TodoCompletedList(generics.ListAPIView):
-    serializer_class = serializers.TodoSerializer
-    permission_class = [permissions.IsAuthenticated]
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
-        Todo.objects.filter(user=user, datecomplete__isnull=False)
+        Todo.objects.filter(user=user, datecompleted__isnull=False).order_by('-datecompleted')
+
+
+class TodoListCreate(generics.ListCreateAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user, datecompleted__isnull=True)
+
