39ccbea6502b58d9f0ba04e01e6b1f1c839bc3ea
rhaertel <renanhaertel@gmail.com>
2021-11-28 18:28:37 -0300

    Adds signup and login urls

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #Todos
     path('todos', views.TodoListCreate.as_view()),
     path('todo/<int:pk>', views.TodoRetrieveUpdateDestroy.as_view()),
     path('todo/<int:pk>/complete', views.TodoComplete.as_view()),
     path('todos/completed', views.TodoCompletedList.as_view()),
+    
+    #Auth
+    path('signup', views.signup),
+    path('login', views.login),
+
+
 ]
