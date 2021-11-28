90ee4572b83eb9c763639c8e9611406cb3ced0cc
rhaertel <renanhaertel@gmail.com>
2021-11-28 11:04:01 -0300

    Adds TodoCompletedList to api views Adds TodoCompletedList logic to display all completed todos for a user

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from todo import serializers
+from todo.models import Todo
 
-# Create your views here.
+class TodoCompletedList(generics.ListAPIView):
+    serializer_class = serializers.TodoSerializer
+    permission_class = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        Todo.objects.filter(user=user, datecomplete__isnull=False)
